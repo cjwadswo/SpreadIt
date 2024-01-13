b0bd94a0a896bd1f648772fd6f54d88659d16bc2
ElementalTJ <elementaltj@gmail.com>
2024-01-13 14:28:06 -0700

    0.01

--- Minigames/Weed_Killer/scripts/weapon.gd
+++ Minigames/Weed_Killer/scripts/weapon.gd
@@ -6,6 +6,7 @@ func _ready():
 
 func _process(delta):
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		global_position.y = get_global_mouse_position().y - 75
+		global_position.y = get_global_mouse_position().y
 		global_position.x = get_global_mouse_position().x
-	self.rotation = position.angle_to(get_global_mouse_position())
+		#global_position.x = lerp(position.x, get_global_mouse_position().x, 0.5)
+	self.rotation = lerp(position.angle_to(get_global_mouse_position()),get_global_mouse_position().x/20, 0.1)
