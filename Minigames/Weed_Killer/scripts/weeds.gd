d78f325f5df7b1de51f5fb6f14c4453301373713
ElementalTJ <elementaltj@gmail.com>
2024-01-13 17:40:00 -0700

    0.01

--- Minigames/Weed_Killer/scripts/weeds.gd
+++ Minigames/Weed_Killer/scripts/weeds.gd
@@ -12,9 +12,11 @@ func _process(delta):
 	scale = scale.lerp(target_scale, scale_speed * delta)
 
 func take_damage():
+	$Sound.play()
 	health -= 1
 	scale.x -= 0.1
 	scale.y -= 0.1
+	$CPUParticles2D.emitting = true
 	if health <= 0:
 		queue_free()
 		Global.score += 1
@@ -22,4 +24,4 @@ func take_damage():
 func _on_body_entered(body):
 	if body.is_in_group("Weapon"):
 		take_damage()
-		$Sound.play()
+
