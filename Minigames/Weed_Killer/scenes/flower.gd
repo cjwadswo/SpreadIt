f328f9c2d4b63d43a9ace667c29266935a260f81
ElementalTJ <elementaltj@gmail.com>
2024-01-22 09:34:42 -0700

    0.01

--- Minigames/Weed_Killer/scenes/flower.gd
+++ Minigames/Weed_Killer/scenes/flower.gd
@@ -1,11 +1,14 @@
 extends Area2D
+var startvar = 0
 
 func _ready():
 	$Sprite.pause()
 
 func _process(delta):
+	startvar += 0.5 * delta
 	var overlapping_areas = get_overlapping_areas()
-	if overlapping_areas.size() < 1:
-		$Sprite.play()
-		Global.score += 1
-		modulate = Color.WHITE
+	if startvar > 1:
+		if overlapping_areas.size() < 1:
+			$Sprite.play()
+			Global.score += 1
+			modulate = Color.WHITE
--- Minigames/Weed_Killer/scripts/global.gd
+++ Minigames/Weed_Killer/scripts/global.gd
@@ -1,12 +1,13 @@
 extends Node
 
 var score = 0
+var game_over = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if game_over == true:
+		print("game over")
