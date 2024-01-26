9481693280f6c09529a534c530b0e7421932dba5
cjwadswo <cjwadswo@uno.edu>
2024-01-26 11:58:24 -0600

    Fixed

--- Minigames/Weed_Killer/scripts/main.gd
+++ Minigames/Weed_Killer/scripts/main.gd
@@ -4,6 +4,20 @@ func _ready():
 	Global.game_over = false
 	Global.has_won = false
 
+func game_won():
+	Global.score += 1
+	Global.game_over = true
+	Global.has_won = true
+	print("game won")
+
+func game_lost():
+	Global.game_over = true
+	Global.has_won = true
+	print("game lost")
+
+func _on_timer_out_of_time():
+	game_lost()
+
 func _process(delta):
 	if Global.speed <= 2.0 and Global.weed_killer_score == 3:
 			game_won()
