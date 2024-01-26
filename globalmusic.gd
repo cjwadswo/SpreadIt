1c84fb0d5ef26350d1d2f349b25cbf21033a6e48
ElementalTJ <elementaltj@gmail.com>
2024-01-26 15:48:22 -0700

    Music Loops after done playing

--- globalmusic.gd
+++ globalmusic.gd
@@ -1,12 +1,10 @@
 extends AudioStreamPlayer
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	play_music()
-	
-func _process(delta):
-	pass
+
+func _on_finished():
+	play_music()
 
 func play_music():
 	play()
