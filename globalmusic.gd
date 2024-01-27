14856e7bde381bb96b4d983c0c7f5b05f5a1dd86
cjwadswo <cjwadswo@uno.edu>
2024-01-26 18:21:00 -0600

    Revert "Music Loops after done playing"
    This reverts commit 1c84fb0d5ef26350d1d2f349b25cbf21033a6e48.

--- globalmusic.gd
+++ globalmusic.gd
@@ -1,10 +1,12 @@
 extends AudioStreamPlayer
 
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	play_music()
-
-func _on_finished():
-	play_music()
+	
+func _process(delta):
+	pass
 
 func play_music():
 	play()
